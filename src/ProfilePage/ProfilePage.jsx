a0eb4784bb49b4e8c92fb66a9ff1709386db5273
JASantia85 <JuanASantiago1058@gmail.com>
2022-07-17 20:57:20 -0400

    sets branch for merge

--- src/ProfilePage/ProfilePage.jsx
+++ src/ProfilePage/ProfilePage.jsx
@@ -1,32 +1,11 @@
-import { useState, useEffect } from 'react'
-import * as profileService from '../services/profileService'
 
 const ProfilePage = () => {
-  console.log(useEffect)
-  const [profiles, setProfiles] = useState([])
-
-  useEffect(() => {
-    const fetchProfiles = async () => {
-      const profileData = await profileService.getAllProfiles()
-      setProfiles(profileData)
-    }
-    fetchProfiles()
-  }, [])
 
   return (
     <>
       <h1>Welcome to </h1>
-      <div>
-      {profiles.length ? 
-        <>
-          {profiles.map(profile =>
-            <h1>Welcome to </h1>
-          )}
-        </>
-      :
-        <p>No profiles yet</p>
-      }
-      </div>
+      
+
     </>
   );
 }
