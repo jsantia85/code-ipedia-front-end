42aba9f23e4bbd06970f898b2ed1e0c9af46f55d
Nicholas Bailey <nickcyg1992@gmail.com>
2022-07-19 15:59:29 -0500

    create basic landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,23 @@
 import styles from './Landing.module.css'
+import { Link } from 'react-router-dom'
 
 const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
-    </main>
+    <>
+      {user ?
+        <main className={styles.container}>
+          <h1>hello, {user.name}</h1>
+        </main>
+        :
+        <main className={styles.container}>
+          <section className='landing'>
+          <img alt="landing-img" className="img" src='/landing.png'></img>
+          <Link to="/login">Log In</Link>
+          <Link to="/signup">Sign Up</Link>
+          </section>
+        </main>
+      }
+    </>
   )
 }
 
