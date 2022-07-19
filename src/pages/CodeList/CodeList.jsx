e54786916c70d2b631e5e1fd04ede861a372fabc
Minji <dudvnd13@gmail.com>
2022-07-19 07:21:28 -0700

    update

--- src/pages/CodeList/CodeList.jsx
+++ src/pages/CodeList/CodeList.jsx
@@ -1,4 +1,4 @@
-import styles from './codeList.css'
+// import styles from './codeList.css'
 import { useState } from 'react'
 
 import DisplayCodes from './DisplayCodes'
@@ -12,7 +12,7 @@ console.log(categories)
 const [category, setCategory] = useState('HTML')
 
   return (
-    <div className={styles.code}>
+    <div className='code-list'>
       <h1>Code List</h1>
       <select onChange={(e)=>setCategory(e.target.value)}>
         {categories.map((c,idx) => (
