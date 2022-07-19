28d475cf159e6a12ba6d5e1f635ad1db86c84652
Minji <dudvnd13@gmail.com>
2022-07-19 13:57:09 -0700

    update css

--- src/pages/CodeList/Code.jsx
+++ src/pages/CodeList/Code.jsx
@@ -3,8 +3,11 @@ const Code = (props) => {
       <div className="code-card">
         <section>
           <span>
+            <p class='categoryName'>Title:</p>
             <p id="code-name">{props.post.title}</p>
+            <p class='categoryName'>Category:</p>
             <p id="code-caption">{props.post.category[0].category}</p>
+            <p class='categoryName'>Code:</p>
             <p id="code-category">{props.post.code}</p>
           </span>
         </section>
