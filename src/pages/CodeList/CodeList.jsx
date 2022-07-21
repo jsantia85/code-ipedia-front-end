9fe2d8d270c8cd59ab896b3f7772efcd224ad345
Minji <dudvnd13@gmail.com>
2022-07-21 07:10:10 -0700

    update

--- src/pages/CodeList/CodeList.jsx
+++ src/pages/CodeList/CodeList.jsx
@@ -2,32 +2,29 @@ import styles from './codeList.css'
 import { useState } from 'react'
 
 import DisplayCodes from './DisplayCodes'
-import CodeNav from './CodeNav'
-// import { codes } from '../Index/data'
 
 const CodeList = (props) => {
-console.log(props)
-const categories = [...new Set(props.posts?.map((p) => p?.category[0]?.category))]
-console.log(categories)
-const [category, setCategory] = useState('HTML')
+  console.log(props)
+  const categories = [...new Set(props.posts?.map((p) => p?.category[0]?.category))]
+  console.log(categories)
+  const [category, setCategory] = useState('HTML')
 
   return (
     <div className='code-list'>
       <h1>Code List</h1>
-      <select className={styles.selectBox} onChange={(e)=>setCategory(e.target.value)}>
-        {categories.map((c,idx) => (
-          <option value={c} key={idx}>
-            {c}
-          </option>
-        ))}
-      </select>
+        <select className={styles.selectBox} onChange={(e)=>setCategory(e.target.value)}>
+          {categories.map((c,idx) => (
+            <option value={c} key={idx}>
+              {c}
+            </option>
+          ))}
+        </select>
       <h4>Now viewing {category}</h4>
-      <div className="cardCode">
-      <section>
-        <DisplayCodes category={category} posts={props.posts}/>
-      </section>
-
-    </div>
+        <div className="cardCode">
+          <section>
+            <DisplayCodes category={category} posts={props.posts}/>
+          </section>
+        </div>
     </div>
   )
 }
